f890afdb5c762ac5429138c68a0627e2fe7da65c
Hossam El-Deen <hossameldeenfci@gmail.com>
2017-09-04 21:47:01 +0200

    Added tests to demo

--- src/demo.erl
+++ src/demo.erl
@@ -4,4 +4,14 @@
 %% API
 -export([double/1]).
 
-double(X) -> X * 2.
+double(X) ->
+  X * 2.
+
+%% EUnit Test
+-include_lib("eunit/include/eunit.hrl").
+
+double_test() ->
+  ?assert(double(12) =:= 24),
+  ?assert(double(0) =:= 0),
+  ?assert(double(111111111111111111111) =:= 222222222222222222222),
+  ?assert(double(1.0) =:= 2.0).
