6bd9b2998ebf6a7f6124e70a71b51eb349a6ab9b
Hossam El-Deen <hossameldeenfci@gmail.com>
2017-11-05 03:11:55 +0200

    ms enhancement: slave doesn't have to return any info when it dies.

--- src/ms.erl
+++ src/ms.erl
@@ -19,19 +19,21 @@ master(init, N) ->
   One_to_N = lists:seq(1, N),
   Slaves_pids = lists:map(fun(I) -> start_slave(I) end, One_to_N),
   N_to_pid = maps:from_list(lists:zip(One_to_N, Slaves_pids)),
-  master(listening, N_to_pid);
+  Pid_to_N = maps:from_list(lists:zip(Slaves_pids, One_to_N)),
+  master(listening, N_to_pid, Pid_to_N).
 
-master(listening, N_to_pid) ->
+master(listening, N_to_pid, Pid_to_N) ->
   % Yes, it's bad that I may forget a `master(...)` at the end of one clause. Have a structure in mind to prevent that
   % but to try it in another problem isA. Same for `slave(...)`
   receive
-    {'EXIT', _, N} ->
+    {'EXIT', Pid, _} ->
+      N = maps:get(Pid, Pid_to_N),
       New_pid = start_slave(N),
       io:fwrite("master restarting dead slave~w~n", [N]),
-      master(listening, maps:update(N, New_pid, N_to_pid));
+      master(listening, maps:update(N, New_pid, N_to_pid), maps:put(New_pid, N, maps:remove(Pid, Pid_to_N)));
     {Message, N} ->
       maps:get(N, N_to_pid) ! Message,
-      master(listening, N_to_pid)
+      master(listening, N_to_pid, Pid_to_N)
   end.
 
 start_slave(N) ->
@@ -42,7 +44,7 @@ start_slave(N) ->
 slave(N) ->
   receive
     die ->
-      exit(N);
+      exit(unexpected_crash);
     Msg ->
       io:fwrite("Slave ~w got message ~w~n", [N, Msg]),
       slave(N)
